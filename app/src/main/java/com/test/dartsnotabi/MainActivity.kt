9cc93896843a76666a79f0f5ba41e4d2691d4e11
hidemitsu sekiyama <hidemitsu.sekiyama@gmail.com>
2019-12-07 12:12:16 +0900

    ちょっと進んだ

--- app/src/main/java/com/test/dartsnotabi/MainActivity.kt
+++ app/src/main/java/com/test/dartsnotabi/MainActivity.kt
@@ -1,24 +1,43 @@
 package com.test.dartsnotabi
 
 import android.os.AsyncTask
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.github.kittinunf.fuel.httpGet
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
-
-    val URL = "https://webservice.recruit.co.jp/manabi/station/v2?prefecture=13&key=3e2b4a21beb30b57&format=json&count=1"
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var loadButton:Button = this.findViewById(R.id.main_load)
-        loadButton.setOnClickListener(this)
+        val button: Button = findViewById(R.id.button) as Button
+        button.setOnClickListener {
+            MyAsyncTask().execute()
+        }
     }
 
-    override fun onClick(v: View?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    inner class MyAsyncTask : AsyncTask<Void, Int, Void>() {
+
+        override fun onPreExecute() {
+            Thread.sleep(800)
+        }
+
+        override fun doInBackground(vararg param: Void?): Void? {
+            val triple = "https://webservice.recruit.co.jp/manabi/station/v2?prefecture=13&key=3e2b4a21beb30b57&format=json&count=1&start=101".httpGet().response()
+            println("result is ：" + String(triple.second.data))
+            return null
+        }
+
+        override fun onProgressUpdate(vararg values: Int?) {
+            // do nothing
+        }
+
+        override fun onPostExecute(result: Void?) {
+
+        }
+
     }
 }
