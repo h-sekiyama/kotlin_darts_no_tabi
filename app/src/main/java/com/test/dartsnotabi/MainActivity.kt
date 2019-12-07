f1a190e49b417391a5420d1db4563c4bad2057e9
hidemitsu sekiyama <hidemitsu.sekiyama@gmail.com>
2019-12-07 14:46:20 +0900

    とりあえず駅名取得はできた

--- app/src/main/java/com/test/dartsnotabi/MainActivity.kt
+++ app/src/main/java/com/test/dartsnotabi/MainActivity.kt
@@ -1,33 +1,59 @@
 package com.test.dartsnotabi
 
+import android.annotation.SuppressLint
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.os.AsyncTask
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.fuel.json.responseJson
+import com.github.kittinunf.result.Result
+import org.json.JSONObject
+import com.fasterxml.jackson.databind.ObjectMapper
+import java.io.IOException
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var stationName: String
+    lateinit var stationText: TextView
+    var randomInt: Int = 0
+    lateinit var resultJson: JSONObject
+    lateinit var accessor: HttpAccessor
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val button: Button = findViewById(R.id.button) as Button
-        button.setOnClickListener {
+        stationText = findViewById(R.id.station_textview)
+        val updateButton: Button = findViewById(R.id.button)
+        updateButton.setOnClickListener {
             MyAsyncTask().execute()
+        }
+
+        val copyButton: Button = findViewById(R.id.copy)
+        copyButton.setOnClickListener {
+            val clip: ClipData = ClipData.newPlainText("simple text", stationName)
         }
     }
 
+    @SuppressLint("StaticFieldLeak")
     inner class MyAsyncTask : AsyncTask<Void, Int, Void>() {
 
         override fun onPreExecute() {
-            Thread.sleep(800)
+            Thread.sleep(500)
         }
 
+        @SuppressLint("WrongThread")
         override fun doInBackground(vararg param: Void?): Void? {
-            val triple = "https://webservice.recruit.co.jp/manabi/station/v2?prefecture=13&key=3e2b4a21beb30b57&format=json&count=1&start=101".httpGet().response()
-            println("result is ：" + String(triple.second.data))
+            randomInt = Random.nextInt(935)
+            accessor = HttpAccessor()
+            resultJson =
+                accessor.getJson("https://webservice.recruit.co.jp/manabi/station/v2?prefecture=13&key=3e2b4a21beb30b57&format=json&count=1&start=$randomInt")
+            stationName = readJsonNode(resultJson.toString())
             return null
         }
 
@@ -36,8 +62,35 @@ class MainActivity : AppCompatActivity() {
         }
 
         override fun onPostExecute(result: Void?) {
-
+            stationText.text = stationName
         }
 
+    }
+}
+
+@Throws(IOException::class)
+private fun readJsonNode(json: String): String {
+    val mapper = ObjectMapper()
+
+    // JSON文字列を読み込み、JsonNodeオブジェクトに変換（Fileやbyte[]も引数に取れる）
+    val root = mapper.readTree(json)
+
+    return root.get("results").get("station").get(0).get("name").toString()
+}
+
+class HttpAccessor {
+    fun getJson(url: String): JSONObject {
+        val (_, _, result) = url.httpGet().responseJson()
+
+        return when (result) {
+            is Result.Failure -> {
+                val ex = result.getException()
+
+                JSONObject(mapOf("results" to ex.toString()))
+            }
+            is Result.Success -> {
+                result.get().obj()
+            }
+        }
     }
 }
